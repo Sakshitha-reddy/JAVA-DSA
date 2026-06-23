1007dae0192723a96db2ffd52dc46ee282719719
Sakshitha-reddy <sakshithareddy111@gmail.com>
2026-06-23 20:12:50 +0530

    pattern 19

---
+++ patterns/pattern19.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class pattern19 {
+	public static void main (String[] args) {
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+        
+        for(int i=0;i<n;i++){
+            for(char ch=(char)('A'+n-1-i);ch<=(char)('A'+n-1);ch++){
+                System.out.print(ch);
+            }
+        
+            System.out.println();
+
+ }
+		}
+    }
