ae76fe345374a578ed6374e6365458ec8bb13794
Sakshitha-reddy <sakshithareddy111@gmail.com>
2026-08-08 22:40:29 +0530

    age2

---
+++ ac-java/age2.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class age2 {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int age=sc.nextInt();
+        if(age>18){
+            System.out.println("you are eligible to vote");
+        }else if(age==18){
+            System.out.println("you are not eligible to vote");
+        }else{
+            System.out.println("you are not eligible to vote");
+        }
+    }
+}
