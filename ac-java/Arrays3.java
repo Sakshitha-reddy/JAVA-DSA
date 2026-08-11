7e306e47dee5f11ffae605ef9e773f7ca24c04ee
Sakshitha-reddy <sakshithareddy111@gmail.com>
2026-08-11 21:53:41 +0530

    arrays4

--- ac-java/Arrays3.java
+++ ac-java/Arrays3.java
@@ -3,7 +3,7 @@ public class Arrays3 {
     public static void main(String[] args){
         int marks[]={15,17,10};
         
-        for(int i=0;i<=marks.length-1;i++){
+        for(int i=0;i<marks.length;i++){
             System.out.println(marks[i]);
         }
     }
