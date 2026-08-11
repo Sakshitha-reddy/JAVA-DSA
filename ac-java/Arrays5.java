69214f7f735a258defdb91b6425774d545ee7d47
Sakshitha-reddy <sakshithareddy111@gmail.com>
2026-08-11 21:57:24 +0530

    arrays5

---
+++ ac-java/Arrays5.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Arrays5 {
+    public static void main(String[] args){
+        
+        Scanner sc=new Scanner(System.in);
+        int size=sc.nextInt();
+        int marks[]=new int[size];
+        for(int i=0;i<size;i++){
+            marks[i]=sc.nextInt();
+        }
+        for(int i=0;i<size;i++){
+            System.out.println(marks[i]);
+        }
+    }
+}
+
