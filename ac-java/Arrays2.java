467503d54d9abc7ad0b1beb584ed43c40be44151
Sakshitha-reddy <sakshithareddy111@gmail.com>
2026-08-11 21:38:55 +0530

    arrays2

---
+++ ac-java/Arrays2.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Arrays2 {
+    public static void main(String[] args){
+        int marks[]=new int[3];
+        marks[0]=97;
+        marks[1]=98;
+        marks[2]=95;
+        for(int i=0;i<=marks.length-1;i++){
+            System.out.println(marks[i]);
+        }
+    }
+}
