edad0b74619e41db2c0e602c85341e4df9158eb3
Sakshitha-reddy <sakshithareddy111@gmail.com>
2026-08-14 19:27:00 +0530

    array hw 1

---
+++ ac-java/arrayshw1.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class arrayshw1 {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int size=sc.nextInt();
+        String arr[]=new String[size];
+        for(int i=0;i<size;i++){
+            arr[i]=sc.next();
+        }
+       for(int i=0;i<arr.length;i++){
+        System.out.println(arr[i]);
+       }
+    }
+}
