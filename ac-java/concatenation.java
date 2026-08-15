2c2c47ab704d8b1e90db6ba98b02d04256e37ae5
Sakshitha-reddy <sakshithareddy111@gmail.com>
2026-08-15 22:46:18 +0530

    string concatenation

---
+++ ac-java/concatenation.java
@@ -0,0 +1,9 @@
+
+public class concatenation {
+    public static void main(String[] args){
+        String firstName="Sakshi";
+        String lastName="Reddy";
+        String fullName=firstName+" "+lastName;
+        System.out.println(fullName);
+    }
+}
