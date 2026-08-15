98ee71b128b591405e7707dccc35e01417ad1272
Sakshitha-reddy <sakshithareddy111@gmail.com>
2026-08-15 22:49:00 +0530

    string length

---
+++ ac-java/stringlength.java
@@ -0,0 +1,8 @@
+public class stringlength {
+    public static void main(String[] args){
+        String firstName="Sakshi";
+        String lastName="Reddy";
+        String fullName=firstName+" "+lastName;
+        System.out.println(fullName.length());
+    }
+}
