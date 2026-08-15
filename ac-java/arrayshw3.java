fce6ffecc988a14dfa01a7f05a03d454a40662f6
Sakshitha-reddy <sakshithareddy111@gmail.com>
2026-08-15 19:33:41 +0530

    arrays hw 3

---
+++ ac-java/arrayshw3.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class arrayshw3 {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int size=sc.nextInt();
+        int arr[]=new int[size];
+        for(int i=0;i<size;i++){
+            arr[i]=sc.nextInt();
+        }
+           boolean isAscending = true;
+        for(int i=0;i<arr.length-1;i++){
+            if(arr[i]>arr[i+1]){
+                isAscending = false;
+                break;
+            }
+        }
+        if(isAscending){
+            System.out.println("The array is sorted in ascending order");
+        }else{
+            System.out.println("The array is not sorted in ascending order");
+        }
+    }
+}
