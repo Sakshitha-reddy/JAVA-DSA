43beccdbecca3463e45d08fdc7c548c40ea01a01
Sakshitha-reddy <sakshithareddy111@gmail.com>
2026-08-22 18:24:06 +0530

    stringBuilder

---
+++ ac-java/stringbuilder1.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class stringbuilder1 {
+    public static void main(String[] args){
+        StringBuilder sb=new StringBuilder("Tony");
+        System.out.println(sb);
+        //char at index 0
+        System.out.println(sb.charAt(0));
+        //set char at index 0
+        sb.setCharAt(0,'P');
+        System.out.println(sb);
+        //to insert a char
+        sb.insert(0,'S');
+        System.out.println(sb);
+    }
+}
