fc30a1738d9fcf8d2878c8057c43a554d0d95cfe
Sakshitha-reddy <sakshithareddy111@gmail.com>
2026-08-22 18:33:15 +0530

    sb length

---
+++ ac-java/sb4.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class sb4 {
+    public static void main(String[] args){
+        StringBuilder sb= new StringBuilder("Tony");
+        //append
+        sb.append("s");
+        sb.append("t");
+        sb.append("a");
+        sb.append("r");
+        sb.append("k");
+        System.out.println(sb.length());
+    }
+}
